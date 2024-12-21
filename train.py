c3617815f606cc79c13bf3524d58ee2aff94fc7a
Bhabuk _B <ftraveller23@gmail.com>
2024-12-21 19:33:35 +0545

    coc basic automation

--- train.py
+++ train.py
@@ -16,7 +16,7 @@ def click(x, y):
     win32api.SetCursorPos((x, y))
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
     time.sleep(0.1)  # This pauses the script for 0.1 seconds
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 1)
 
 
 while keyboard.is_pressed('q') == False:
